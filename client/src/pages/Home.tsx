0dde04210ddd207208a39e8610ca612e9d7f7219
Elliott Record <elliottrecord@gmail.com>
2025-06-20 14:03:30 +0100

    - Updated marketing images

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -43,8 +43,8 @@ const Home = () => {
       {/* Hero Section */}
       <section id="hero" className="bg-primary text-white py-16 md:py-24">
         <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex flex-col md:flex-row items-center">
-            <div className="md:w-1/2 mb-10 md:mb-0">
+          <div className="flex flex-col lg:flex-row items-center">
+            <div className="lg:w-1/2 mb-10 lg:mb-0 order-2 lg:order-1">
               <h1 className="text-4xl md:text-5xl font-bold mb-4">Never lose track of your belongings again</h1>
               <p className="text-lg md:text-xl mb-8 text-gray-100">
                 Locrate helps you organise your possessions with virtual boxes and QR codes, making finding them as simple as scanning with your mobile phone.
@@ -72,13 +72,30 @@ const Home = () => {
                 </Button>
               </div>
             </div>
-            <div className="md:w-1/2 flex justify-center">
-              <img
-                src="images/App_Crates_Page.png"
-                alt="Locrate app on smartphone"
-                className="rounded-xl shadow-lg w-3/4 h-auto max-w-sm"
-              />
+
+            <div className="lg:w-1/2 order-1 lg:order-2 ">
+              <div className="flex flex-col sm:flex-row lg:flex-col xl:flex-row gap-6 justify-center items-center">
+                <div className="w-full flex justify-center">
+                  <img
+                    src="images/app-crates.jpg"
+                    alt="Locrate App Home Page"
+                    className="rounded-xl w-full h-auto max-w-sm"
+                  />
+                </div>
+              </div>
+
             </div>
+            <div className="lg:w-1/2 hidden lg:block order-3 ">
+              <div className=" flex flex-col sm:flex-row lg:flex-col xl:flex-row gap-6 justify-center items-center">
+
+                <div className="w-full flex justify-center">
+                  <img
+                    src="images/app-print-qr.jpg"
+                    alt="Locrate Print QR Code Example"
+                    className="rounded-xl w-full h-auto max-w-sm"
+                  />
+                </div>
+              </div></div>
           </div>
         </div>
       </section>
@@ -257,7 +274,7 @@ const Home = () => {
               <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 justify-center lg:justify-start ">
                 <a
                   onClick={handleStoreClick("App Store")}
-                  className="bg-black hover:bg-gray-900 text-white px-6 py-3 rounded-lg font-medium flex items-center justify-center"
+                  className="bg-black hover:bg-gray-900 text-white px-6 py-3 rounded-lg font-medium flex items-center justify-center cursor-pointer"
                 >
                   <svg viewBox="0 0 384 512" width="24" height="24" className="mr-3" fill="currentColor">
                     <path d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z" />
@@ -269,7 +286,7 @@ const Home = () => {
                 </a>
                 <a
                   onClick={handleStoreClick("Google Play")}
-                  className="bg-black hover:bg-gray-900 text-white px-6 py-3 rounded-lg font-medium flex items-center justify-center"
+                  className="bg-black hover:bg-gray-900 text-white px-6 py-3 rounded-lg font-medium flex items-center justify-center cursor-pointer"
                 >
                   <svg viewBox="0 0 512 512" width="24" height="24" className="mr-3" fill="currentColor">
                     <path d="M325.3 234.3L104.6 13l280.8 161.2-60.1 60.1zM47 0C34 6.8 25.3 19.2 25.3 35.3v441.3c0 16.1 8.7 28.5 21.7 35.3l256.6-256L47 0zm425.2 225.6l-58.9-34.1-65.7 64.5 65.7 64.5 60.1-34.1c18-14.3 18-46.5-1.2-60.8zM104.6 499l280.8-161.2-60.1-60.1L104.6 499z" />
@@ -284,12 +301,17 @@ const Home = () => {
             <div className="lg:w-1/2">
               <div className="flex flex-col md:flex-row justify-center items-center gap-6">
                 <img
-                  src="images/App_Fancy_Dress.png"
+                  src="images/app-search.jpg"
                   alt="Locrate app scanning QR code"
                   className="rounded-xl shadow-lg w-3/4 lg:w-1/2 h-auto max-w-sm"
                 />
                 <img
-                  src="images/App_Search.png"
+                  src="images/app-locate.jpg"
+                  alt="Locrate app search"
+                  className="rounded-xl shadow-lg w-3/4 lg:w-1/2 h-auto max-w-sm"
+                />
+                <img
+                  src="images/app-scan-qr.jpg"
                   alt="Locrate app search"
                   className="rounded-xl shadow-lg w-3/4 lg:w-1/2 h-auto max-w-sm"
                 />
